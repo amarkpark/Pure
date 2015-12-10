84dca51686e83a5d68f58f7177d14bc0fd02c666
amarkpark <nospam@amarkpark.com>
2015-12-10 01:47:19 +0000

    Initial commit. ~AMP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pure::Application.config.secret_key_base = '18865ad1d5144c5b6aae9f9d9b32f6a1d7ba2db1ab934d680099ce57ea6d28ee2d06194fbd347d42f305ad94113475cab1e67d147f9e50ec7275a1876402d3e4'
